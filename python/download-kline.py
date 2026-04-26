c52f28be99271602fb77abeab57f39667b8429f7
ilia <vdogrex@gmail.com>
2026-04-26 13:29:04 +0300

    kline

--- python/download-kline.py
+++ python/download-kline.py
@@ -189,7 +189,7 @@ if __name__ == "__main__":
 
     print(dates)
 
-    start_date = '2026-01-14'
+    start_date = '2018-01-01'
 
     asyncio.run(download_daily_klines('spot', symbols, num_symbols, ["5m"], dates, start_date, None, folder))
 
